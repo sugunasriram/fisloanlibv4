5cf140ad2e72649ef208a55f8b36172fb756a7fc
Suguna Sriram <haisugusri@gmail.com>
2026-03-02 22:25:00 +0530

    Dowgraded to 2.1.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,20 @@ plugins {
 //    id ("com.google.firebase.crashlytics") version "3.0.2" apply false
 
 
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10" apply false
-    id("com.android.application") version "8.12.3" apply false
-    id("com.google.gms.google-services") version "4.4.4" apply false
-    id("com.google.firebase.crashlytics") version "3.0.6" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
+//    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
+//    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10" apply false
+//    id("com.android.application") version "8.12.3" apply false
+//    id("com.google.gms.google-services") version "4.4.4" apply false
+//    id("com.google.firebase.crashlytics") version "3.0.6" apply false
+//    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
+
+        id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20" apply false
+        id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
+        id("com.android.application") version "8.12.3" apply false
+        id("com.android.library") version "8.12.3" apply false
+        id("com.google.gms.google-services") version "4.4.4" apply false
+        id("com.google.firebase.crashlytics") version "3.0.6" apply false
 
     id("maven-publish") // ✅ needed for publishing
 }
